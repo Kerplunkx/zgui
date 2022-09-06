06f721740b24ba4e7d5b7162a91d890ddf079334
artemisx64 <jobinsjjc9312@protonmail.com>
2022-09-06 18:56:36 +0900

    Separate Backends

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn linkImgui(exe: *std.build.LibExeObjStep) void {
+pub fn zgui(exe: *std.build.LibExeObjStep) std.build.Pkg {
     exe.linkLibCpp();
     exe.addIncludeDir(comptime thisDir() ++ "/libs");
     exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui.cpp", &.{""});
@@ -9,31 +9,36 @@ pub fn linkImgui(exe: *std.build.LibExeObjStep) void {
     exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_draw.cpp", &.{""});
     exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_demo.cpp", &.{""});
     exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/zgui.cpp", &.{""});
-    exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_glfw.cpp", &.{""});
-    exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_opengl3.cpp", &.{""});
-    exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_vulkan.cpp", &.{""});
+    return .{
+        .name = "zgui",
+        .source = .{ .path = comptime thisDir() ++ "/src/zgui.zig" },
+    };
 }
 
 fn thisDir() []const u8 {
     return std.fs.path.dirname(@src().file) orelse ".";
 }
 
-pub const pkg = std.build.Pkg{
-    .name = "zgui",
-    .source = .{ .path = thisDir() ++ "/src/zgui.zig" },
-};
+pub fn opengl(exe: *std.build.LibExeObjStep) std.build.Pkg {
+    exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_opengl3.cpp", &.{""});
+    return .{
+        .name = "zgui_opengl",
+        .source = .{ .path = comptime thisDir() ++ "/src/zgui_opengl.zig" },
+    };
+}
 
-pub const opengl = std.build.Pkg{
-    .name = "zgui_opengl",
-    .source = .{ .path = thisDir() ++ "/src/zgui_opengl.zig" },
-};
+pub fn glfw(exe: *std.build.LibExeObjStep) std.build.Pkg {
+    exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_glfw.cpp", &.{""});
+    return .{
+        .name = "zgui_glfw",
+        .source = .{ .path = comptime thisDir() ++ "/src/zgui_glfw.zig" },
+    };
+}
 
-pub const glfw = std.build.Pkg{
-    .name = "zgui_glfw",
-    .source = .{ .path = thisDir() ++ "/src/zgui_glfw.zig" },
-};
-
-pub const vulkan = std.build.Pkg{
-    .name = "zgui_vulkan",
-    .source = .{ .path = thisDir() ++ "/src/zgui_vulkan.zig" },
-};
+pub fn vulkan(exe: *std.build.LibExeObjStep) std.build.Pkg {
+    exe.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_vulkan.cpp", &.{""});
+    return .{
+        .name = "zgui_vulkan",
+        .source = .{ .path = comptime thisDir() ++ "/src/zgui_vulkan.zig" },
+    };
+}
