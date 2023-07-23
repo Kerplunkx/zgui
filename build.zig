328e871f76b9c6124b6b536081ef1202b52e4674
Guillermo Moreira <guillermomoreira01@gmail.com>
2023-07-23 17:20:20 -0500

    fixed typo

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn zgui(b: *std.Build, step: *std.Build.Step.Copile) std.Build.Module {
+pub fn zgui(b: *std.Build, step: *std.Build.Step.Compile) std.Build.Module {
     step.linkLibCpp();
     step.addIncludePath(comptime thisDir() ++ "/libs");
     step.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui.cpp", &.{""});
@@ -16,21 +16,21 @@ pub fn zgui(b: *std.Build, step: *std.Build.Step.Copile) std.Build.Module {
 
 fn thisDir() []const u8 {}
 
-pub fn opengl(b: *std.Build, step: *std.Build.Step.Copile) std.Build.Module {
+pub fn opengl(b: *std.Build, step: *std.Build.Step.Compile) std.Build.Module {
     step.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_opengl3.cpp", &.{""});
     return b.createModule(.{
         .source = .{ .path = comptime thisDir() ++ "/src/zgui_opengl.zig" },
     });
 }
 
-pub fn glfw(b: *std.Build, step: *std.Build.Step.Copile) std.Build.Module {
+pub fn glfw(b: *std.Build, step: *std.Build.Step.Compile) std.Build.Module {
     step.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_glfw.cpp", &.{""});
     return b.createModule(.{
         .source = .{ .path = comptime thisDir() ++ "/src/zgui_glfw.zig" },
     });
 }
 
-pub fn vulkan(b: *std.Build, step: *std.Build.Step.Copile) std.Build.Module {
+pub fn vulkan(b: *std.Build, step: *std.Build.Step.Compile) std.Build.Module {
     step.addCSourceFile(comptime thisDir() ++ "/libs/imgui/imgui_impl_vulkan.cpp", &.{""});
     return b.createModule(.{
         .source = .{ .path = comptime thisDir() ++ "/src/zgui_vulkan.zig" },
